4403f7e395cb88f5720d0441f61fbca52568641c
varshanayak123 <vn8646964@gmail.com>
2026-08-11 00:08:42 +0530

    Add Second largest element array problem

---
+++ src/com/Arrays/SecondLargestElement.java
@@ -0,0 +1,21 @@
+package com.Arrays;
+
+public class SecondLargestElement {
+    public static void main(String[] args ) {
+        int[] arr = {2,4,5,9,10,7};
+        int largest = arr[0];
+        int SecLargest = arr[0];
+        for(int i=1 ; i< arr.length ; i++) {
+            if (arr[i] > largest) {
+                SecLargest = largest;
+                largest = arr[i];
+            } else if (SecLargest < arr[i] && arr[i] != largest) {
+                arr[i] = SecLargest;
+            }
+        }
+            System.out.println("Secound Largest: "+SecLargest);
+
+        }
+
+    }
+
