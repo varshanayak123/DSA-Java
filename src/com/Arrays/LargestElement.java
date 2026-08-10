613d3a38cee8161fa92d687d4d2ebd6c3344c0aa
varshanayak123 <vn8646964@gmail.com>
2026-08-10 22:19:47 +0530

    Added code

---
+++ src/com/Arrays/LargestElement.java
@@ -0,0 +1,15 @@
+package com.Arrays;
+
+public class LargestElement {
+    public static void main(String[] args) {
+        int[] arr = {4, 6, 7, 10, 8};
+        int largest = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > largest) {
+                largest = arr[i];
+            }
+        }
+            System.out.println("Largest no is: " + largest);
+
+    }
+}
