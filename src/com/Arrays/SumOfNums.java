7217ef6b15a41bf0dd689c9fb3dcb2a5d68b4c34
varshanayak123 <vn8646964@gmail.com>
2026-08-16 22:39:29 +0530

    Added

---
+++ src/com/Arrays/SumOfNums.java
@@ -0,0 +1,17 @@
+package com.Arrays;
+
+import java.util.Scanner;
+
+public class SumOfNums {
+    public static void main (String []args){
+        Scanner sc = new Scanner(System.in);
+        int a  = sc.nextInt();
+        int sum = 0;
+        while(a>0){
+            int rem = a % 10;
+            sum = sum +rem;
+            a = a/10;
+        }
+        System.out.println(sum);
+    }
+}
